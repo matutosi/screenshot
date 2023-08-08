e5166a68efaf1ca83357673a6e89158575bd3e97
Toshikazu Matsumura <matutosi@gmail.com>
2023-08-08 15:47:47 +0900

    first commit

---
+++ R/install_screenshot.R
@@ -0,0 +1,18 @@
+#' 
+#' 
+#' 
+#' 
+#' 
+#' 
+#' 
+#' 
+#' 
+#' 
+install_screenshot <- function(){
+  
+  
+}
+
+git remote add origin https://github.com/matutosi/screenshot.git
+git branch -M main
+git push -u origin main
