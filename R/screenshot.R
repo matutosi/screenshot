45573c2b7c17e3eca00ebd3fcfd013e0f1a3fa78
Toshikazu Matsumura <matutosi@gmail.com>
2023-08-09 14:29:26 +0900

    update

--- R/screenshot.R
+++ R/screenshot.R
@@ -1,27 +1,31 @@
 #' Take a screenshot. 
 #' 
-#' Need to install screenCapture on Win by install_screenshot().
+#' Need to install screenshot.exe on Win by install_screenshot().
 #' 
-#' @param file  A string for file name of screenshot.
-#' @seealso     install_screenshot()
-#' @return      A file name of screenshot. When "", screenshot will be saved in a tempral directory.
+#' @param bin_dir  A string for directory name of screenshot.exe on Win.
+#' @param file     A string for file name of screenshot.
+#' @seealso        install_screenshot()
+#' @return         A file name of screenshot. When "", screenshot will be saved in a tempral directory.
 #' @examples
 #' \donttest{
 #' library(imager)
 #' sc <- screenshot()
-#' imager::load.image(sc)
+#' sc_image <- imager::load.image(sc)
+#' plot(sc_image)
 #' }
 #' 
 #' @export
-screenshot <- function(file = ""){
+screenshot <- function(bin_dir = "", file = ""){
   if(file == ""){
     file <- fs::file_temp("sc_", ext = "png")
   }
   os <- get_os()
   if(os == "win"){
-    pkg <- fs::path_package("screenshot")
+    if(bin_dir == ""){
+      bin_dir <- fs::path_package("screenshot")
+    }
     exe <- "screenshot.exe"
-    cmd <- paste0(fs::path(pkg, exe), " ", file)
+    cmd <- paste0(fs::path(bin_dir, exe), " ", file)
   }else if(os == "mac"){
     exe <- "screencapture -o"
     cmd <- paste0(fs::path(exe), " ", file)
@@ -36,14 +40,15 @@ screenshot <- function(file = ""){
 #' Codes are from URL shown below.
 #' https://superuser.com/questions/75614/take-a-screen-shot-from-command-line-in-windows#answer-1751844
 #' 
-#' @param dir A string of direcotory to be installed.
+#' @param dir A string of directory to be installed.
 #' @return     A string of installed dir.
 #' 
 #' @examples
 #' \donttest{
 #' library(fs)
 #' dir <- fs::path_package("screenshot")
-#' # dir <- "SET_YOUR DIRECTORY"
+#' # if you want to install another directory
+#' #   dir <- "SET_YOUR DIRECTORY"
 #' install_screenshot(dir)
 #' }
 #' 
