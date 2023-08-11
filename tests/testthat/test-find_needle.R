42d9ec9c3ebcaa970684388849c8648f15af185f
Toshikazu Matsumura <matutosi@gmail.com>
2023-08-11 12:49:49 +0900

    test find needle image

---
+++ tests/testthat/test-find_needle.R
@@ -0,0 +1,27 @@
+devtools::load_all("D:/matu/work/ToDo/screenshot/.")
+library(magrittr)
+
+testthat::test_that("locate_position() can find needle image", {
+  located_position_of_needle_image_by_locate_position_function <- function(example, pos_x, pos_y){
+    haystack_image <- imager::load.example(example)
+    hay_mt <- image2gray_matrix(haystack_image)
+    ndl_mt <- 
+      haystack_image %>%
+      hay2needle(pos_x, pos_y) %>%
+      image2gray_matrix()
+    xy <- locate_ndl_in_hay(ndl_mt, hay_mt, exact = FALSE)
+    return(xy)
+  }
+
+  examples <- c("hubble", "birds", "parrots")
+  positions <- c(101, 201, 301)
+  for(example in examples){
+    for(pos in positions){
+    image <- imager::load.example(example)
+    expect_equal(
+      c(pos, pos),
+      located_position_of_needle_image_by_locate_position_function(example, pos, pos))
+    }
+  }
+
+})
